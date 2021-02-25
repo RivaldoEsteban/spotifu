fcefbc002f9b2ddd7e3612041022585557e1534c
Rivaldo Esteban <63760024+RivaldoEsteban@users.noreply.github.com>
2021-02-25 16:06:06 -0600

    modal-home

--- songs-list.js
+++ songs-list.js
@@ -309,7 +309,6 @@ function remplazarSong3() {
   imageSong.src = song3.cover;
   nameSong.textContent = song3.name
   artista.textContent = song3.artist
-  // reproduction1Controls.innerHTML = '<audio src="./music/3.mp3" id="audio3"  autoplay controls></audio>'
   changeColor()
   reproducir1(audio3, icon3)
    audio1.pause();
@@ -840,3 +839,60 @@ icon11.addEventListener ('clik' , remplazarSong11)
 icon12.addEventListener ('clik' , remplazarSong12)
 icon13.addEventListener ('clik' , remplazarSong13)
 icon14.addEventListener ('clik' , remplazarSong14)
+
+
+const home = document.getElementById('home')
+const explorar = document.getElementById('explorar')
+const radio = document.getElementById('radio')
+const borde1 = document.getElementById('borde1')
+const borde2 = document.getElementById('borde2')
+const borde3 = document.getElementById('borde3')
+
+home.addEventListener('click' , () => {
+  home.style.color ="white"
+  home.style.opacity = '1'
+  explorar.style.color = 'gray'
+  radio.style.color = 'gray'
+  borde1.style.borderLeft = '3px solid #8d038d'
+  borde1.style.paddingLeft = '10px'
+  borde2.style.borderLeft = 'none'
+  borde2.style.paddingLeft = 'none'
+  borde3.style.borderLeft = 'none'
+  borde3.style.paddingLeft = 'none'
+
+  borde1.style.filter = "invert(100%)"
+  borde2.style.filter = "invert(80%)"
+  borde3.style.filter = "invert(80%)"
+})
+
+explorar.addEventListener('click' , () => {
+  explorar.style.color ="white"
+  home.style.color = "gray"
+  radio.style.color = "gray"
+  borde2.style.borderLeft = '3px solid#8d038d'
+  borde2.style.paddingLeft = '10px'
+  borde1.style.borderLeft = 'none'
+  borde1.style.paddingLeft = 'none'
+  borde3.style.borderLeft = 'none'
+  borde3.style.paddingLeft = 'none'
+  borde2.style.filter = "invert(100%)"
+  borde3.style.filter = "invert(80%)"
+  borde1.style.filter = "invert(80%)"
+
+})
+
+radio.addEventListener('click' , () => {
+  radio.style.color ="white"
+  home.style.color = "gray"
+  explorar.style.color = "gray"
+  borde1.style.borderLeft = 'none'
+  borde1.style.paddingLeft = 'none'
+  borde2.style.borderLeft = 'none'
+  borde2.style.paddingLeft = 'none'
+  borde3.style.borderLeft = '3px solid #8d038d'
+  borde3.style.paddingLeft = '10px'
+  borde3.style.filter = "invert(100%)"
+  borde2.style.filter = "invert(100%)"
+  borde1.style.filter = "invert(100%)"
+})
+
